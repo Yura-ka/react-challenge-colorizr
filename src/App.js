7d91323f17101becd4a047fd62afa57c9bce0360
Yura-ka <ka.prosto-ka@yandex.ru>
2016-06-01 01:35:11 +0300

    beginning app development on react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/main.scss';
 
 export default () => (
   <div className="container">
+
     <h1 className="title"> Basic Counter Example</h1>
     <Counter />
   </div>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Button from '../Button/Button';
+import './navbar.scss';
+
+export default class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+}
